413f054e790f36694d832221faa8a28b78390937
harshavardhant <harshavardhan.t@mobisy.com>
2020-06-26 12:26:39 +0530

    Updates

---
+++ precache-manifest.a668e53905d251d0163903643925a6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308b20a4346e550338fb2defda037bee",
+    "url": "/91Solutions/index.html"
+  },
+  {
+    "revision": "06297c04d41ceb30eb1e",
+    "url": "/91Solutions/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8a48afb1d0bef425a3c8",
+    "url": "/91Solutions/static/js/2.118f1584.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/91Solutions/static/js/2.118f1584.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06297c04d41ceb30eb1e",
+    "url": "/91Solutions/static/js/main.fa422128.chunk.js"
+  },
+  {
+    "revision": "672325da6c6440f56d3b",
+    "url": "/91Solutions/static/js/runtime-main.b6e28940.js"
+  }
+]);
